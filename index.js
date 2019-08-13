bfb716126089f7c536bdb9e3635d5c7ec47b194e
Sean Altemose <sean@seanaltemose.com>
2019-08-13 23:05:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,19 @@ function showResults(json) {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const token = getToken()
+  fetch('https://api.github.com/learn-co-curriculum/javascript-fetch-lab/forks', {
+    method: 'post',
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json())
+  .then(json => showResults(json) );
   //use fetch to fork it!
 }
 
 function getToken() {
+  const token = '';
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
   return ''
